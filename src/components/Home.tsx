9867cc352b1168c42aaabdefdb4b9c8638b4ca55
codeeon <yeon.develop@gmail.com>
2023-12-04 21:18:35 +0900

    style: main page hot contents

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import HotContents from './main/HotContents';
+import styled from 'styled-components';
 
-const Home = () => {
-  return <div></div>;
+const Home: React.FC = () => {
+  return (
+    <Base>
+      <HotContents />
+    </Base>
+  );
 };
 
 export default Home;
+
+const Base = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  box-sizing: border-box;
+`;
