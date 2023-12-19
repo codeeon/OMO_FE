91f58a31ff4b00cebbba5127a5b6b0f4ce7bd4ce
codeeon <yeon.develop@gmail.com>
2023-12-19 15:00:11 +0900

    Feat: authorization with axios interceptors

--- src/axios/authApi.ts
+++ src/axios/authApi.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import auth from './auth';
 
-const instance = axios.create({
+const instance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVER_URL,
   withCredentials: true,
 });
@@ -10,18 +11,46 @@ instance.interceptors.request.use(
     const accessToken = localStorage.getItem('accessToken');
 
     if (accessToken) {
-      // 액세스 토큰이 있다면 헤더에 추가
-      config.headers.Authorization = `Bearer ${accessToken}`;
+      config.headers.Authorization = `${accessToken}`;
     }
 
     return config;
   },
+
   (error) => {
     return Promise.reject(error);
   },
-  function () {},
 );
 
-instance
+instance.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error) => {
+    // req는 에러가 발생한 axios 요청 (config 객체)
+    const req = error.config;
+
+    // _retry는 axios interceptor의 커스텀 플래그, 재시도 된 요청에 인터셉터 로직이 실행되는 것을 방지함
+    if (error.response?.status === 401 && !req._retry) {
+      req._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        const refreshResponse = await auth.post(
+          '/tokens/refresh',
+          { refreshToken },
+          { withCredentials: true },
+        );
+
+        req.headers['Authorization'] = `${refreshResponse.data.accessToken}`;
+        return instance(req);
+      } catch (refreshError) {
+        console.error('토큰 새로 고침 실패:', refreshError);
+        throw refreshError;
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
