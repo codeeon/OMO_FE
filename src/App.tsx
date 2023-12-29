af1f833d253fe5beae3bb0ec156d056c0c360378
nkc930604@gmail.com <nkc930604@gmail.com>
2023-12-29 10:19:41 +0900

    Feat: "add ASKII Code"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routers from './shared/Routers';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
@@ -7,6 +7,26 @@ import useThemeStore from './store/theme/themeStore.js';
 
 const App = () => {
   const { themeMode, toggleTheme } = useThemeStore();
+  useEffect(() => {
+    console.log(`
+    OOOOOOOOO     MMMMMMMM               MMMMMMMM     OOOOOOOOO     
+    OO:::::::::OO   M:::::::M             M:::::::M   OO:::::::::OO   
+  OO:::::::::::::OO M::::::::M           M::::::::M OO:::::::::::::OO 
+ O:::::::OOO:::::::OM:::::::::M         M:::::::::MO:::::::OOO:::::::O
+ O::::::O   O::::::OM::::::::::M       M::::::::::MO::::::O   O::::::O
+ O:::::O     O:::::OM:::::::::::M     M:::::::::::MO:::::O     O:::::O
+ O:::::O     O:::::OM:::::::M::::M   M::::M:::::::MO:::::O     O:::::O
+ O:::::O     O:::::OM::::::M M::::M M::::M M::::::MO:::::O     O:::::O
+ O:::::O     O:::::OM::::::M  M::::M::::M  M::::::MO:::::O     O:::::O
+ O:::::O     O:::::OM::::::M   M:::::::M   M::::::MO:::::O     O:::::O
+ O:::::O     O:::::OM::::::M    M:::::M    M::::::MO:::::O     O:::::O
+ O::::::O   O::::::OM::::::M     MMMMM     M::::::MO::::::O   O::::::O
+ O:::::::OOO:::::::OM::::::M               M::::::MO:::::::OOO:::::::O
+  OO:::::::::::::OO M::::::M               M::::::M OO:::::::::::::OO 
+    OO:::::::::OO   M::::::M               M::::::M   OO:::::::::OO   
+      OOOOOOOOO     MMMMMMMM               MMMMMMMM     OOOOOOOOO     
+    `);
+  }, []);
 
   return (
     <ThemeProvider theme={themeMode === 'LightMode' ? LightTheme : DarkTheme}>
