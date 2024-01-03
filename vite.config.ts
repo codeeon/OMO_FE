5f7affd3f5f4f1d5b09833990e819f70fe4f7f14
nkc930604@gmail.com <nkc930604@gmail.com>
2024-01-03 19:58:54 +0900

    Fix: "fix font preload"
    폰트 preload 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite';
+import { HtmlTagDescriptor, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
+
+const fontsDirectory = resolve(__dirname, 'public/assets/fonts');
+const fontFiles = readdirSync(fontsDirectory).filter((file) =>
+  file.endsWith('.woff2'),
+);
+
+const injectFontsToHead: HtmlTagDescriptor[] = fontFiles.map((fontFile) => ({
+  injectTo: 'head',
+  tag: 'link',
+  attrs: {
+    rel: 'preload',
+    href: `/assets/fonts/${fontFile}`,
+    as: 'font',
+    type: 'font/woff2',
+    crossOrigin: 'anonymous',
+  },
+}));
 
 export default defineConfig({
   plugins: [
@@ -10,6 +30,11 @@ export default defineConfig({
     VitePluginHtmlEnv(),
     VitePluginHtmlEnv({
       compiler: true,
+    }),
+    createHtmlPlugin({
+      inject: {
+        tags: injectFontsToHead,
+      },
     }),
   ],
   define: {
